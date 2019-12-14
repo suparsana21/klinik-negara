10ff4d3761441fc3b8756e4c8ddd626bd06fe5f2
Made Suparsana <madesuparsana29@gmail.com>
2019-12-14 09:05:47 +0800

    Update fucking shitty

--- KlinikBerseri/Form1.cs
+++ KlinikBerseri/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//awaokwaowkoakwokawok
 namespace KlinikBerseri
 {
     public partial class Form1 : Form
