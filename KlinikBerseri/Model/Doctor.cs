2113fe265a517d0d4de3b5304608c50a9a8b3c4d
Made Suparsana <madesuparsana29@gmail.com>
2019-12-14 08:52:55 +0800

    Remove Fucking Shit

--- KlinikBerseri/Model/Doctor.cs
+++ KlinikBerseri/Model/Doctor.cs
@@ -8,5 +8,12 @@ namespace KlinikBerseri.Model
 {
     class Doctor
     {
+        string id { get; set;}
+        string name { get; set;}
+        string address  { get; set;}
+        string phone { get; set;}
+        string specialist { get; set; }
     }
+
+
 }
